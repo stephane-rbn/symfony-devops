63a0a22e59104dd11f96c77f34f7cf001f3ebf25
Stéphane <rabena.stephane@gmail.com>
2020-10-01 14:44:13 +0200

    fix

--- src/Controller/WebsiteController.php
+++ src/Controller/WebsiteController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annot ation\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class WebsiteController extends AbstractController
 {
